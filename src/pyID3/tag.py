22fba99932cde08f6449a577b27ad86232049ac1
Green Wang <cocowool@qq.com>
2018-06-22 14:27:57 +0800

    add chardet module to detect charset

--- src/pyID3/tag.py
+++ src/pyID3/tag.py
@@ -1,6 +1,8 @@
 # Read ID3v1 tag information
 import os
 import string
+import base64
+import chardet
 
 def parse(fileObj, version = 'v1'):
     fileObj.seek(0,2)
@@ -10,24 +12,50 @@ def parse(fileObj, version = 'v1'):
     fileObj.seek(-128,2)
     tag_data = fileObj.read()
 
-    if(id3v_data[0:3] != b'TAG'):
+    if(tag_data[0:3] != b'TAG'):
         return False
     
     return getTag(tag_data)
 
+# Detect the encoding and decode
+def decodeData(bin_seq):
+    # print(bin_seq)
+    result = chardet.detect(bin_seq)
+    # print(result)
+    if(result['confidence'] > 0):
+        try:
+            return bin_seq.decode(result['encoding'])
+        except UnicodeDecodeError:
+            return 'Decode Failed'
+
+
 # Get ID3v1 tag data
 def getTag(tag_data):
+    # STRIP_CHARS = compat.b(string.whitespace) + b"\x00"
+    STRIP_CHARS = b"\x00"
+
     tags = {}
-    tags['title'] = tag_data[3,33]
+    tags['title'] = tag_data[3:33].strip(STRIP_CHARS)
 
-    tags['artist'] = tag_data[33:63]
+    if(tags['title']):
+        tags['title'] = decodeData(tags['title'])
 
-    tags['album'] = tag_data[63:93]
+    tags['artist'] = tag_data[33:63].strip(STRIP_CHARS)
+    if(tags['artist']):
+        tags['artist'] = decodeData(tags['artist'])
 
-    tags['year'] = tag_data[93:97]
+    tags['album'] = tag_data[63:93].strip(STRIP_CHARS)
+    if(tags['album']):
+        tags['album'] = decodeData(tags['album'])
 
-    tags['comment'] = tag_data[97:127]
+    tags['year'] = tag_data[93:97].strip(STRIP_CHARS)
+    # if(tags['year']):
+    #     tags['year'] = decodeData(tags['year'])
+
+    tags['comment'] = tag_data[97:127].strip(STRIP_CHARS)
     #@TODO Need to analyze comment to verfiy v1 or v1.1
+    if(tags['comment']):
+        tags['comment'] = decodeData(tags['comment'])
 
     tags['genre'] = ord(tag_data[127:128])
 
---
+++ src/pyID3/utils/charset.py
@@ -0,0 +1,1 @@
+# Detect the charset
