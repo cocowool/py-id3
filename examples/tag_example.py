fd7be434e2620ea632e32e4e8d2113d8bafb7bae
Shiqiang <cocowool@qq.com>
2022-06-05 08:56:59 +0800

    modify mp3tag code

--- examples/tag_example.py
+++ examples/tag_example.py
@@ -1,23 +1,38 @@
 import sys
 
 # 这里改为自己本机 pyID3 的存储目录
-sys.path.insert(0, '~/Project/py-id3/src/pyID3')
+sys.path.append('/Users/shiqiang/Projects/py-id3/src/mp3tag')
+sys.path.append('/Users/shiqiang/Projects/test-python-package/tagreader')
 
-from pyID3 import pyID3
-import os
+for p in sys.path:
+    print(p)
 
-print(sys.path)
-path = "/Users/rousseau/shiqiang/Music/iTunes/iTunes Media/Music"
+import tagreader as tr
+tr.hello()
 
-def findMp3(path):
-    files = os.listdir(path)
-    for f in files:
-        if( os.path.isdir(os.path.join(path,f)) ):
-            findMp3( os.path.join(path,f) )
-        else:
-            fh = open(os.path.join(path,f), "rb")
-            print(tag.parse(fh))
+# import mp3tag
+import reader as mpr
 
-# print(tag)
+# import pyid3
 
-findMp3(path)
+mpr.hello()
+
+
+# from pyID3 import tag
+# import os
+
+# print(sys.path)
+# path = "/Users/rousseau/shiqiang/Music/iTunes/iTunes Media/Music"
+
+# def findMp3(path):
+#     files = os.listdir(path)
+#     for f in files:
+#         if( os.path.isdir(os.path.join(path,f)) ):
+#             findMp3( os.path.join(path,f) )
+#         else:
+#             fh = open(os.path.join(path,f), "rb")
+#             print(tag.parse(fh))
+
+# # print(tag)
+
+# findMp3(path)
